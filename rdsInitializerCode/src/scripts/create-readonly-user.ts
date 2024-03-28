e7a6630107e25f64950b5de77919b95abedf194e
Malik Mahmud <almalikmahmud@gmail.com>
2024-03-28 20:46:45 +0100

    fix: enquote db password

--- rdsInitializerCode/src/scripts/create-readonly-user.ts
+++ rdsInitializerCode/src/scripts/create-readonly-user.ts
@@ -9,14 +9,14 @@ interface Config {
 const ROLE_NAME = "readaccess";
 
 async function main(db: Kysely<unknown>, config: Config) {
-  console.log(`- Creating readonly user: ${config.username} for database: ${config.dbname}.`);
+  console.log(`- Creating readonly user: "${config.username}" for database: "${config.dbname}".`);
 
   await db.transaction().execute(async (trx) => {
     await sql.raw(`CREATE ROLE ${ROLE_NAME}`).execute(trx);
     await sql.raw(`GRANT CONNECT ON DATABASE ${config.dbname} to ${ROLE_NAME}`).execute(trx);
     await sql.raw(`GRANT USAGE ON SCHEMA public TO ${ROLE_NAME}`).execute(trx);
     await sql.raw(`GRANT SELECT ON ALL TABLES IN SCHEMA public TO ${ROLE_NAME}`).execute(trx);
-    await sql.raw(`CREATE USER ${config.username} WITH PASSWORD ${config.password}`).execute(trx);
+    await sql.raw(`CREATE USER ${config.username} WITH PASSWORD '${config.password}'`).execute(trx);
     await sql.raw(`GRANT ${ROLE_NAME} TO ${config.username}`).execute(trx);
   });
 
