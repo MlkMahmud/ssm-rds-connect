7bb7bacaa4051a94be9b50abba030ad52e51e711
Malik Mahmud <almalikmahmud@gmail.com>
2024-03-28 20:33:21 +0100

    feat: create readonly db user

--- rdsInitializerCode/src/index.ts
+++ rdsInitializerCode/src/index.ts
@@ -1,9 +1,22 @@
+import { GetSecretValueCommand, SecretsManagerClient } from "@aws-sdk/client-secrets-manager";
 import { CloudFormationCustomResourceEvent, CloudFormationCustomResourceResponse } from "aws-lambda";
-import { getPhysicalResourceId, getSecretValue } from "./utils";
-import migrate from "./scripts/migrate";
 import getDatabaseConnection from "./database";
+import createReadOnlyDbUser from "./scripts/create-readonly-user";
+import migrate from "./scripts/migrate";
 
-const { DB_CREDS_SECRET_NAME = "" } = process.env;
+const { DB_CREDS_SECRET_NAME = "", DB_READONLY_CREDS_SECRET_NAME = "" } = process.env;
+const client = new SecretsManagerClient();
+
+async function getSecretValue<T>(SecretId: string): Promise<T> {
+  const command = new GetSecretValueCommand({ SecretId });
+  const response = await client.send(command);
+
+  if (!response.SecretString) {
+    throw new Error(`Failed to get secret with id: "${SecretId}"`);
+  }
+
+  return JSON.parse(response.SecretString) as T;
+}
 
 interface DatabaseConfig {
   dbname: string;
@@ -17,7 +30,24 @@ export async function handler(event: CloudFormationCustomResourceEvent): Promise
   console.log(event);
 
   switch (event.RequestType) {
-    case "Create":
+    case "Create": {
+      const { dbname, host, password, port, username } = await getSecretValue<DatabaseConfig>(DB_CREDS_SECRET_NAME);
+      const readonlyDbConfig = await getSecretValue<DatabaseConfig>(DB_READONLY_CREDS_SECRET_NAME);
+      const database = getDatabaseConnection(`postgresql://${username}:${password}@${host}:${port}/${dbname}`);
+      await createReadOnlyDbUser(database, readonlyDbConfig);
+      await migrate(database);
+      await database.destroy();
+
+      return {
+        Status: "SUCCESS",
+        LogicalResourceId: event.LogicalResourceId,
+        PhysicalResourceId: event.ServiceToken,
+        RequestId: event.RequestId,
+        StackId: event.StackId,
+        Data: { message: `'Create' event handler completed successfully.` },
+      };
+    }
+
     case "Update": {
       const { dbname, host, password, port, username } = await getSecretValue<DatabaseConfig>(DB_CREDS_SECRET_NAME);
       const database = getDatabaseConnection(`postgresql://${username}:${password}@${host}:${port}/${dbname}`);
@@ -27,11 +57,11 @@ export async function handler(event: CloudFormationCustomResourceEvent): Promise
       return {
         Status: "SUCCESS",
         LogicalResourceId: event.LogicalResourceId,
-        PhysicalResourceId: getPhysicalResourceId(event),
+        PhysicalResourceId: event.PhysicalResourceId,
         RequestId: event.RequestId,
         StackId: event.StackId,
-        Data: { message: `'${event.RequestType}' event handler completed successfully.` }
-      }
+        Data: { message: `'Update' event handler completed successfully.` },
+      };
     }
 
     default: {
@@ -41,8 +71,8 @@ export async function handler(event: CloudFormationCustomResourceEvent): Promise
         PhysicalResourceId: event.PhysicalResourceId,
         RequestId: event.RequestId,
         StackId: event.StackId,
-        Data: { message: `Skipping '${event.RequestType}' event.` }
-      }
+        Data: { message: `Skipping '${event.RequestType}' event.` },
+      };
     }
   }
 }
---
+++ rdsInitializerCode/src/scripts/create-readonly-user.ts
@@ -0,0 +1,26 @@
+import { Kysely, sql } from "kysely";
+
+interface Config {
+  dbname: string;
+  password: string;
+  username: string;
+}
+
+const ROLE_NAME = "readaccess";
+
+async function main(db: Kysely<unknown>, config: Config) {
+  console.log(`- Creating readonly user: ${config.username} for database: ${config.dbname}.`);
+
+  await db.transaction().execute(async (trx) => {
+    await sql.raw(`CREATE ROLE ${ROLE_NAME}`).execute(trx);
+    await sql.raw(`GRANT CONNECT ON DATABASE ${config.dbname} to ${ROLE_NAME}`).execute(trx);
+    await sql.raw(`GRANT USAGE ON SCHEMA public TO ${ROLE_NAME}`).execute(trx);
+    await sql.raw(`GRANT SELECT ON ALL TABLES IN SCHEMA public TO ${ROLE_NAME}`).execute(trx);
+    await sql.raw(`CREATE USER ${config.username} WITH PASSWORD ${config.password}`).execute(trx);
+    await sql.raw(`GRANT ${ROLE_NAME} TO ${config.username}`).execute(trx);
+  });
+
+  console.log("- Done creating readonly user.");
+}
+
+export default main;
