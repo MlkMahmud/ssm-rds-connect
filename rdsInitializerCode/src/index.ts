846b77c6f816532ccb3daa8f407dbb1277634a91
Malik Mahmud <almalikmahmud@gmail.com>
2024-05-17 22:15:46 +0100

    chore:[skip ci] remove redundant script

--- rdsInitializerCode/src/index.ts
+++ rdsInitializerCode/src/index.ts
@@ -33,6 +33,7 @@ export async function handler(event: CloudFormationCustomResourceEvent): Promise
       const { dbname, host, password, port, username } = await getSecretValue<DatabaseConfig>(DB_CREDS_SECRET_NAME);
       const database = getDatabaseConnection(`postgresql://${username}:${password}@${host}:${port}/${dbname}`);
       await migrate(database);
+      await createIAMUsers(database, { username: "malik", dbname });
       await database.destroy();
 
       return {
@@ -49,7 +50,6 @@ export async function handler(event: CloudFormationCustomResourceEvent): Promise
       const { dbname, host, password, port, username } = await getSecretValue<DatabaseConfig>(DB_CREDS_SECRET_NAME);
       const database = getDatabaseConnection(`postgresql://${username}:${password}@${host}:${port}/${dbname}`);
       await migrate(database);
-      await createIAMUsers(database, { username: "malik", dbname });
       await database.destroy();
 
       return {
